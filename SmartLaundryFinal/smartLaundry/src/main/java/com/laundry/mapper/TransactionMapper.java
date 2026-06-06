fbe1d16b39a2635b2973f0c8322ba57b42d127e2
Rruben555 <rubensiregar25@gmail.com>
2026-06-06 14:13:37 +0700

    servicename+weight

--- SmartLaundryFinal/smartLaundry/src/main/java/com/laundry/mapper/TransactionMapper.java
+++ SmartLaundryFinal/smartLaundry/src/main/java/com/laundry/mapper/TransactionMapper.java
@@ -1,30 +1,33 @@
 package com.laundry.mapper;
-
 import com.laundry.dto.response.TransactionResponse;
 import com.laundry.entity.Transaction;
+import com.laundry.entity.TransactionDetail;
 
 public class TransactionMapper {
+    private TransactionMapper() {}
 
-    private TransactionMapper() {
-    }
-
-    public static TransactionResponse toResponse(
-            Transaction transaction
-    ) {
+    public static TransactionResponse toResponse(Transaction transaction) {
+        TransactionDetail firstDetail = 
+            (transaction.getTransactionDetails() != null 
+                && !transaction.getTransactionDetails().isEmpty())
+            ? transaction.getTransactionDetails().get(0)
+            : null;
 
         return TransactionResponse.builder()
                 .id(transaction.getId())
-                .customerName(
-                        transaction.getCustomer().getName()
-                )
-                .employeeName(
-                        transaction.getEmployee().getFullName()
-                )
+                .customerName(transaction.getCustomer().getName())
+                .employeeName(transaction.getEmployee().getFullName())
                 .totalPrice(transaction.getTotalPrice())
                 .status(transaction.getStatus())
                 .paymentStatus(transaction.getPaymentStatus())
                 .pickupDate(transaction.getPickupDate())
                 .createdAt(transaction.getCreatedAt())
+                .serviceName(firstDetail != null
+                    ? firstDetail.getService().getServiceName()
+                    : null)
+                .weight(firstDetail != null
+                    ? firstDetail.getWeight()
+                    : null)
                 .build();
     }
 }
